5c9058b5b8745c6c8a3042f33ea950a6d310b31a
dmarcelin <marcelinthierry@gmail.com>
2025-06-28 02:02:46 -0400

    MN-0012 feat: Add generic dialog for reusability

---
+++ lib/utilities/dialogs/logout_dialog.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:mynotes/utilities/dialogs/generic_dialog.dart';
+
+Future<bool> showLogoutDialog(
+  BuildContext context,
+) {
+  return showGenericDialog<bool>(
+      context: context,
+      title: 'Log out',
+      content: 'Are you sure you want to log out?',
+      optionsBuilder: () => {
+            'Yes': true,
+            'No': false,
+          }).then((value) => value ?? false);
+}
